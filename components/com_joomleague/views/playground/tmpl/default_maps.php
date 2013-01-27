123dff28b98dfb6fbbcbab24b566f22b7b94709a
diddipoeler <famploeger@arcor.de>
2013-01-27 12:02:38 +0100

    google map stadion

--- components/com_joomleague/views/playground/tmpl/default_maps.php
+++ components/com_joomleague/views/playground/tmpl/default_maps.php
@@ -1,41 +1,53 @@
 <?php defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
-
+<div style="width: 100%; float: left">
+	<div class="contentpaneopen">
+		<div class="contentheading">
+			<?php echo JText::_('COM_JOOMLEAGUE_GMAP_DIRECTIONS'); ?>
+		</div>
+	</div>
 <?php
 
-if ( $this->gmap )
-{
-	?>
 
-<h2><?php echo JText::_('COM_JOOMLEAGUE_GMAP_DIRECTIONS'); ?></h2>
-	<div class="venuecontent map">
-		<?php
-		// create the container for map
-		echo $this->gmap->MapHolder();
+    
+
+		$arrPluginParams = array();
 		
-		// write a link to center location in map if is configured
-		if ( $this->mapconfig['center_link'] == 1 )
-		{
-			echo $this->gmap->GetSideClick();
+		$arrPluginParams[] = "zoomWheel='1'";
+		
+		$param = 'default_map_type';
+		if($this->mapconfig[$param]) {
+			$arrPluginParams[] = "mapType='".$this->mapconfig[$param]."'";
 		}
-		if ( $this->config['show_route'] == 1 )
-		{
-			$this->gmap->RouteForm();
+		$param = 'map_control';
+		if($this->mapconfig[$param]) {
+			$arrPluginParams[] = "zoomType='".$this->mapconfig[$param]."'";
 		}
-		?>
-	</div>
-	
-	<?php
-	// insert script tag for google maps api
-	echo $this->gmap->GmapsKey();
-	
-	// write javascript for map creation
-	echo $this->gmap->InitJs();
-	if ( $this->config['show_route'] == 1 )
-	{
-		$this->gmap->InitRouteJs();
-	}
-	
-	// write a javascript to properly unload map when leaving site
-	echo $this->gmap->UnloadMap();
-	}
+		$param = 'width';
+		if($this->mapconfig[$param]) {
+			$arrPluginParams[] = "$param='".$this->mapconfig[$param]."'";
+		}
+		$param = 'height';
+		if($this->mapconfig[$param]) {
+			$arrPluginParams[] = "$param='".$this->mapconfig[$param]."'";
+		}
+		
+		if($this->address_string != '') {
+			$arrPluginParams[] = "address='" .$this->address_string. "'";
+			$arrPluginParams[] = "text='<div style=width:250px;height=30px;>".$this->address_string."</div>'";
+		}
+		$icon = '';
+		if($this->club->logo_small != '')
+		{
+			$arrPluginParams[] = "tooltip='". $this->playground->name . "'";
+			$icon= $this->club->logo_small;
+		}
+		if($icon!='') {
+			$arrPluginParams[] = "icon='".$icon."'";
+		}
+		$params  = '{mosmap ';
+		$params .= implode('|', $arrPluginParams);
+		$params .= "}";
+		echo JHTML::_('content.prepare', $params);
+	    
 ?>
+</div>
