3af91ae515f6183867914b2d43eb0db6e8192889
diddipoeler <famploeger@arcor.de>
2013-02-03 09:56:26 +0100

    ewige tabelle

--- components/com_joomleague/views/rankingalltime/tmpl/default.php
+++ components/com_joomleague/views/rankingalltime/tmpl/default.php
@@ -1,35 +1,18 @@
-<?php defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
+<?php
+defined('_JEXEC') or die('Restricted access');
 
-<?php $this->_addPath( 'template', JPATH_COMPONENT . DS . 'views' . DS . 'footer' . DS . 'tmpl' ); ?>
-<?php $this->_addPath( 'template', JPATH_COMPONENT . DS . 'views' . DS . 'backbutton' . DS . 'tmpl' ); ?>
+// Make sure that in case extensions are written for mentioned (common) views,
+// that they are loaded i.s.o. of the template of this view
+$templatesToLoad = array(
+    'projectheading',
+    'backbutton',
+    'footer');
+JoomleagueHelper::addTemplatePaths($templatesToLoad, $this);
+?>
 
 <div class="joomleague">
-<!-- projectheading -->
+	<?php
+echo $this->loadTemplate('projectheading');
 
-<?PHP
-
-//$this->overallconfig['show_footer'] = 1;
-
-echo '<div class="componentheading">';
-			//echo $this->leaguename;
-			#JoomleagueHTML::PrintIcon( $row, $params, false, '' );
-			#JoomleagueModelProject::PrintIcon( null, null, true, '' );
-			echo '</div>';
 ?>
-
-<?php echo $this->loadTemplate('sectionheader'); ?>
-<?php echo $this->loadTemplate('ranking'); ?>
-<?PHP
-if ($this->config['show_colorlegend']==1){echo $this->loadTemplate('colorlegend');}
-	
-if ($this->config['show_explanation']==1){echo $this->loadTemplate('explanation');}
-?>
-	
-<!-- backbutton -->
-<div>
-<?php echo $this->loadTemplate('backbutton'); ?>
-<!-- footer -->
-<?php echo $this->loadTemplate('footer'); ?>
-</div>
-
 </div>
