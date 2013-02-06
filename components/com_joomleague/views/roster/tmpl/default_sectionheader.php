72929e2f344bea96c4eda2c054fbee0837b6a3df
diddipoeler <famploeger@arcor.de>
2013-02-06 21:08:16 +0100

    roster

--- components/com_joomleague/views/roster/tmpl/default_sectionheader.php
+++ components/com_joomleague/views/roster/tmpl/default_sectionheader.php
@@ -15,11 +15,14 @@
 		?>
 		</td>
         
+        
+        
+		<form name='resultsRoundSelector' method='post'>
+		<input type='hidden' name='option' value='com_joomleague' />
         <?php
-        
-        echo "<td>".JHTML::_('select.genericlist', $this->lists['type'], 'type' , 'class="inputbox" size="1"', 'value', 'text', $this->type )."</td>";
+        echo "<td>".JHTML::_('select.genericlist', $this->lists['type'], 'type' , 'class="inputbox" size="1" onchange="this.form.submit(); ', 'value', 'text', $this->type )."</td>";
         ?>
-        
+        </form>
 	</tr>
 </table>
 <br />
