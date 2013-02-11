53bd9938277688f107449ed6f0a9a16c4eb92d5a
diddipoeler <famploeger@arcor.de>
2013-02-11 08:57:17 +0100

    rss feed

--- components/com_joomleague/views/results/view.html.php
+++ components/com_joomleague/views/results/view.html.php
@@ -95,6 +95,8 @@ if ( ($this->overallconfig['show_project_rss_feed']) == 1 )
 			if (!isset($this->config['show_dnp_teams_icons'])){$this->config['show_dnp_teams_icons']=0;}
 			if (!isset($this->config['show_results_ranking'])){$this->config['show_results_ranking']=0;}
 		}
+        
+        $this->assign('show_debug_info', JComponentHelper::getParams('com_joomleague')->get('show_debug_info',0) );
 		
 		// Set page title
 		$pageTitle = JText::_('COM_JOOMLEAGUE_RESULTS_PAGE_TITLE');
