96f0034b4c5225f554e0ff5a593fb325c38a5c27
diddipoeler <famploeger@arcor.de>
2013-02-12 20:45:53 +0100

    update script

--- administrator/components/com_joomleague/assets/updates/update_version.sql
+++ administrator/components/com_joomleague/assets/updates/update_version.sql
@@ -1,1 +1,1 @@
-update #__joomleague_version set major='2', minor='0', build='44', revision='b3fd04d', version='a', file='joomleague' where id=1
+update #__joomleague_version set major='2', minor='0', build='45', revision='-diddipoeler', version='a', file='joomleague' where id=1
