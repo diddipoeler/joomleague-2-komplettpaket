fb8cb2232804dc50a43892cf4877f47a339be72f
diddipoeler <famploeger@arcor.de>
2013-02-13 22:38:15 +0100

    update version 2.0.47

--- administrator/components/com_joomleague/assets/updates/update_version.sql
+++ administrator/components/com_joomleague/assets/updates/update_version.sql
@@ -1,1 +1,1 @@
-update #__joomleague_version set major='2', minor='0', build='46', revision='-diddipoeler', version='a', file='joomleague' where id=1
+update #__joomleague_version set major='2', minor='0', build='47', revision='-diddipoeler', version='a', file='joomleague' where id=1
