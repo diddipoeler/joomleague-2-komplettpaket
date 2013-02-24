1bbf2b7680740511a43c99c1aabe94e60f722955
diddipoeler <famploeger@arcor.de>
2013-02-24 13:09:39 +0100

    mehrere sportarten löschen

--- administrator/components/com_joomleague/models/sportstype.php
+++ administrator/components/com_joomleague/models/sportstype.php
@@ -31,13 +31,15 @@ class JoomleagueModelSportsType extends JoomleagueModelItem
 	 * @return	boolean	True on success
 	 * @since	0.1
 	 */
-	function delete($cid=array())
+	function delete($pks=array())
 	{
-		$result=false;
-		if (count($cid))
+		$mainframe	=& JFactory::getApplication();
+    $result=false;
+		if (count($pks))
 		{
-			JArrayHelper::toInteger($cid);
-			$cids=implode(',',$cid);
+			//JArrayHelper::toInteger($pks);
+			$cids=implode(',',$pks);
+			$mainframe->enqueueMessage(JText::_('JoomleagueModelSportsType-delete id->'.$cids),'Notice');
 			$query="SELECT COUNT(id) FROM #__joomleague_sports_type";
 			$this->_db->setQuery($query);
 			if ($this->_db->loadResult()==count($cid))
@@ -66,7 +68,7 @@ class JoomleagueModelSportsType extends JoomleagueModelItem
 				$this->setError(JText::_('COM_JOOMLEAGUE_ADMIN_SPORTTYPE_MODEL_ERROR_PROJECT_EXISTS'));
 				return false;
 			}
-			return parent::delete($cids);
+			return parent::delete($pks);
 		}
 		return true;
 	}
