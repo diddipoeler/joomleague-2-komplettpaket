f89b0a569a192974a08ab4449b6fbdecbcdb63c6
diddipoeler <famploeger@arcor.de>
2013-07-08 10:11:53 +0200

    frontend: tabs&slider-plugin im nextmatch eingebaut.

--- components/com_joomleague/views/nextmatch/tmpl/default.php
+++ components/com_joomleague/views/nextmatch/tmpl/default.php
@@ -56,7 +56,47 @@ JoomleagueHelper::addTemplatePaths($templatesToLoad, $this);
 		}
 		else if ( ($this->config['show_nextmatch_tabs']) == "show_tabs" )
 	{
-	$idxTab = 1;
+	
+    if ( JPluginHelper::isEnabled('content', 'jw_ts') )
+    {
+        $params = '';
+    $startoutput = '{tab=';
+    $endoutput = '{/tabs}';
+    if (($this->config['show_details'])==1)
+		{
+    $params .= $startoutput.JText::_('COM_JOOMLEAGUE_NEXTMATCH_DETAILS').'}';
+    $params .= $this->loadTemplate('details');
+    }
+    if (($this->config['show_preview'])==1)
+		{
+    $params .= $startoutput.JText::_('COM_JOOMLEAGUE_NEXTMATCH_PREVIEW').'}';
+    $params .= $this->loadTemplate('preview'); 
+    }
+    if (($this->config['show_stats'])==1)
+		{
+    $params .= $startoutput.JText::_('COM_JOOMLEAGUE_NEXTMATCH_H2H').'}';
+    $params .= $this->loadTemplate('stats');
+    }
+    if (($this->config['show_history'])==1)
+		{
+    $params .= $startoutput.JText::_('COM_JOOMLEAGUE_NEXTMATCH_HISTORY').'}';
+    $params .= $this->loadTemplate('history'); 
+    }
+    if (($this->config['show_previousx'])==1)
+		{
+    $params .= $startoutput.JText::_('COM_JOOMLEAGUE_NEXTMATCH_PREVIOUS_TEAMS').'}';
+    $this->currentteam = $this->match->projectteam1_id;
+    $params .= $this->loadTemplate('previousx');
+    $this->currentteam = $this->match->projectteam2_id;
+    $params .= $this->loadTemplate('previousx');      
+    }
+    $params .= $endoutput;
+    echo JHTML::_('content.prepare', $params);
+
+        }
+        else
+        {
+    $idxTab = 1;
   echo JHTML::_('tabs.start','tabs_nextmatch', array('useCookie'=>1));
 	if (($this->config['show_details'])==1)
 		{
@@ -92,9 +132,56 @@ JoomleagueHelper::addTemplatePaths($templatesToLoad, $this);
 		}
 	
 	echo JHTML::_('tabs.end');
+    }
+    
 	}
 	else if ( ($this->config['show_nextmatch_tabs']) == "show_slider" )
 	{
+	if ( JPluginHelper::isEnabled('content', 'jw_ts') )
+    {
+    $params = '';    
+    $startoutput = '{slider=';
+    $endoutput = '{/slider}';    
+    if (($this->config['show_details'])==1)
+		{
+    $params .= $startoutput.JText::_('COM_JOOMLEAGUE_NEXTMATCH_DETAILS').'}';
+    $params .= $this->loadTemplate('details');
+    $params .= $endoutput;
+    }
+    if (($this->config['show_preview'])==1)
+		{
+    $params .= $startoutput.JText::_('COM_JOOMLEAGUE_NEXTMATCH_PREVIEW').'}';
+    $params .= $this->loadTemplate('preview'); 
+    $params .= $endoutput;
+    }
+    if (($this->config['show_stats'])==1)
+		{
+    $params .= $startoutput.JText::_('COM_JOOMLEAGUE_NEXTMATCH_H2H').'}';
+    $params .= $this->loadTemplate('stats');
+    $params .= $endoutput;
+    }
+    if (($this->config['show_history'])==1)
+		{
+    $params .= $startoutput.JText::_('COM_JOOMLEAGUE_NEXTMATCH_HISTORY').'}';
+    $params .= $this->loadTemplate('history'); 
+    $params .= $endoutput;
+    }
+    if (($this->config['show_previousx'])==1)
+		{
+    $params .= $startoutput.JText::_('COM_JOOMLEAGUE_NEXTMATCH_PREVIOUS_TEAMS').'}';
+    $this->currentteam = $this->match->projectteam1_id;
+    $params .= $this->loadTemplate('previousx');
+    $this->currentteam = $this->match->projectteam2_id;
+    $params .= $this->loadTemplate('previousx'); 
+    $params .= $endoutput;     
+    }    
+        
+        
+        
+        echo JHTML::_('content.prepare', $params);
+        }
+        else
+        {   
 	$idxTab = 1;
   echo JHTML::_('sliders.start','slider_nextmatch', array('useCookie'=>1));
 	if (($this->config['show_details'])==1)
@@ -131,6 +218,8 @@ JoomleagueHelper::addTemplatePaths($templatesToLoad, $this);
 		}
 	
 	echo JHTML::_('sliders.end');
+    }
+    
 	}
 
 		echo "<div>";
