39606a5687f329ccbcffb26f26cba098ed9336ec
diddipoeler <famploeger@arcor.de>
2013-08-14 14:19:25 +0200

    vorbereitung version 2.1.12

---
+++ administrator/components/com_joomleague/views/currentseasons/tmpl/default.php
@@ -0,0 +1,127 @@
+<?php 
+defined('_JEXEC') or die(JText::_('Restricted access'));
+JHTML::_('behavior.tooltip');
+
+
+//echo 'this->items<br /><pre>~' . print_r($this->items,true) . '~</pre><br />';
+
+$path='/administrator/components/com_joomleague/assets/images/';
+
+foreach ($this->items as $item)
+	{
+	echo JHTML::_('sliders.start','sliders',array(
+										'allowAllClose' => true,
+										'startTransition' => true,
+										true));
+			echo JHTML::_('sliders.panel',$item->name,'panel-'.$item->name);
+?>
+
+			<div id="element-box">
+				<div class="t"><div class="t"><div class="t">&nbsp;</div></div></div>
+				<div class="m">
+					<fieldset class="adminform">
+						<legend><?php echo JText::sprintf('COM_JOOMLEAGUE_ADMIN_PROJECTS_CONTROL_PANEL_LEGEND','<i>'.$item->name.'</i>'); ?></legend>
+						<table border='0'>
+							<tr>
+								
+									<div id="cpanel">
+										<?php
+				 						$link=JRoute::_('index.php?option=com_joomleague&task=project.edit&cid[]='.$item->id.'&pid[]='.$item->id  );
+										$text=JText::_('COM_JOOMLEAGUE_P_MENU_PSETTINGS');
+										$imageFile='icon-48-ProjectSettings.png';
+										$linkParams="<span>$text</span>&nbsp;";
+										$image=JHTML::_('image.administrator',$imageFile,$path,NULL,NULL,$text).$linkParams;
+										?>
+										<div class="icon-wrapper"><div class="icon"><?php echo JHTML::link($link,$image); ?></div></div>		
+										<?php
+				 						$link=JRoute::_('index.php?option=com_joomleague&view=templates&task=template.display&pid[]='.$item->id);
+										$text=JText::_('COM_JOOMLEAGUE_P_MENU_FES');
+										$imageFile='icon-48-FrontendSettings.png';
+										$linkParams="<span>$text</span>&nbsp;";
+										$image=JHTML::_('image.administrator',$imageFile,$path,NULL,NULL,$text).$linkParams;
+										?>
+										<div class="icon-wrapper"><div class="icon"><?php echo JHTML::link($link,$image); ?></div></div>
+										<?php
+										if ((isset($item->project_type)) &&
+											($item->project_type == 'DIVISIONS_LEAGUE'))
+										{
+											$link=JRoute::_('index.php?option=com_joomleague&view=divisions&task=division.display&pid[]='.$item->id);
+											$text=JText::_('COM_JOOMLEAGUE_P_MENU_DIVISIONS');
+											$imageFile='icon-48-Divisions.png';
+											$linkParams="<span>$text</span>&nbsp;";
+											$image=JHTML::_('image.administrator',$imageFile,$path,NULL,NULL,$text).$linkParams;
+										?>
+											<div class="icon-wrapper"><div class="icon"><?php echo JHTML::link($link,$image); ?></div></div>
+										<?php
+										}
+										if ((isset($item->project_type)) &&
+											(($item->project_type == 'TOURNAMENT_MODE') ||
+											 ($item->project_type == 'DIVISIONS_LEAGUE')))
+										{
+											$link=JRoute::_('index.php?option=com_joomleague&view=treetos&task=treeto.display&pid[]='.$item->id);
+											$text=JText::_('COM_JOOMLEAGUE_P_MENU_TREE');
+											$imageFile='icon-48-Tree.png';
+											$linkParams="<span>$text</span>&nbsp;";
+											$image=JHTML::_('image.administrator',$imageFile,$path,NULL,NULL,$text).$linkParams;
+											?>
+											<div class="icon-wrapper"><div class="icon"><?php echo JHTML::link($link,$image); ?></div></div>
+										<?php
+										}
+										$link=JRoute::_('index.php?option=com_joomleague&view=projectposition&task=projectposition.display&pid[]='.$item->id);
+										$text=JText::_('COM_JOOMLEAGUE_P_MENU_POSITIONS');
+										$imageFile='icon-48-Positions.png';
+										$linkParams="<span>$text</span>&nbsp;";
+										$image=JHTML::_('image.administrator',$imageFile,$path,NULL,NULL,$text).$linkParams;
+										?>
+										<div class="icon-wrapper"><div class="icon"><?php echo JHTML::link($link,$image); ?></div></div>
+										<?php
+										$link=JRoute::_('index.php?option=com_joomleague&view=projectreferees&task=projectreferee.display&pid[]='.$item->id);
+										$text=JText::_('COM_JOOMLEAGUE_P_MENU_REFEREES');
+										$imageFile='icon-48-Referees.png';
+										$linkParams="<span>$text</span>&nbsp;";
+										$image=JHTML::_('image.administrator',$imageFile,$path,NULL,NULL,$text).$linkParams;
+										?>
+										<div class="icon-wrapper"><div class="icon"><?php echo JHTML::link($link,$image); ?></div></div>
+										<?php
+				 						$link=JRoute::_('index.php?option=com_joomleague&view=projectteams&task=projectteam.display&pid[]='.$item->id);
+										$text=JText::_('COM_JOOMLEAGUE_P_MENU_TEAMS');
+										$imageFile='icon-48-Teams.png';
+										$linkParams="<span>$text</span>&nbsp;";
+										$image=JHTML::_('image.administrator',$imageFile,$path,NULL,NULL,$text).$linkParams;
+										?>
+										<div class="icon-wrapper"><div class="icon"><?php echo JHTML::link($link,$image); ?></div></div>
+										<?php
+				 						$link=JRoute::_('index.php?option=com_joomleague&view=rounds&task=round.display&pid[]='.$item->id);
+										$text=JText::_('COM_JOOMLEAGUE_P_MENU_MATCHDAYS');
+										$imageFile='icon-48-Matchdays.png';
+										$linkParams="<span>$text</span>&nbsp;";
+										$image=JHTML::_('image.administrator',$imageFile,$path,NULL,NULL,$text).$linkParams;
+										?>
+										<div class="icon-wrapper"><div class="icon"><?php echo JHTML::link($link,$image); ?></div></div>
+										<?php
+				 						$link=JRoute::_('index.php?option=com_joomleague&view=jlxmlexports&task=jlxmlexport.display&pid[]='.$item->id);
+										$text=JText::_('COM_JOOMLEAGUE_P_MENU_XML_EXPORT');
+										$imageFile='icon-48-XMLExportData.png';
+										$linkParams="<span>$text</span>&nbsp;";
+										$image=JHTML::_('image.administrator',$imageFile,$path,NULL,NULL,$text).$linkParams;
+										?>
+										<div class="icon-wrapper"><div class="icon"><?php echo JHTML::link($link,$image); ?></div></div>
+									</div>
+								
+							</tr>
+						</table>
+					</fieldset>
+				</div>
+				<div class="m">
+					<fieldset class="adminform">
+						<table><tr><td><div id="cpanel"><?php echo JText::_('COM_JOOMLEAGUE_ADMIN_PROJECTS_CONTROL_PANEL_HINT'); ?></div></td></tr></table>
+					</fieldset>
+				</div>
+				<div class="b"><div class="b"><div class="b"></div></div></div>
+			</div>
+			<!-- bottom close main table opened in default_admin -->
+<?PHP                        
+            echo JHTML::_('sliders.end');   
+    }   
+
+?>
---
+++ administrator/components/com_joomleague/views/currentseasons/view.html.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @copyright	Copyright (C) 2006-2013 JoomLeague.net. All rights reserved.
+ * @license		GNU/GPL,see LICENSE.php
+ * Joomla! is free software. This version may have been modified pursuant
+ * to the GNU General Public License,and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+jimport('joomla.application.component.view');
+
+/**
+ * HTML View class for the Joomleague component
+ *
+ * @static
+ * @package	JoomLeague
+ * @since	0.1
+ */
+class JoomleagueViewCurrentseasons extends JLGView
+{
+	function display($tpl=null)
+	{
+		$option 	= JRequest::getCmd('option');
+		$mainframe	= JFactory::getApplication();
+		$uri		= JFactory::getUri();
+        // Get data from the model
+		$items		= $this->get('Data');
+        $this->assignRef('items', $items);
+        
+        foreach ($this->items as $item)
+	{
+	   
+       }
+
+$this->addToolbar();
+		parent::display($tpl);
+	}
+
+	/**
+	* Add the page title and toolbar.
+	*
+	* @since	1.6
+	*/
+	protected function addToolbar()
+	{ 
+	// Set toolbar items for the page
+		JToolBarHelper::title(JText::_('COM_JOOMLEAGUE_ADMIN_PROJECTS_TITLE'),'ProjectSettings');
+		JToolBarHelper::divider();
+		
+		JLToolBarHelper::onlinehelp();
+		JToolBarHelper::preferences(JRequest::getCmd('option'));
+	}
+}
+?>
