e9a92d4caeac204b83aea0122a40bf03a5277c56
diddipoeler <famploeger@arcor.de>
2013-09-01 15:47:54 +0200

    vorbereitung neues menü.

---
+++ administrator/components/com_joomleague/models/cpanel.php
@@ -0,0 +1,201 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+// import Joomla modelform library
+jimport('joomla.application.component.model');
+ 
+/**
+ * 
+ */
+class joomleagueModelcpanel extends JModel
+{
+
+public function getVersion() 
+	{
+	   $mainframe =& JFactory::getApplication();
+	   $this->_db->setQuery('SELECT manifest_cache FROM #__extensions WHERE name = "com_sportsmanagement"');
+       $manifest_cache = json_decode( $this->_db->loadResult(), true );
+	   //$mainframe->enqueueMessage(JText::_('manifest_cache<br><pre>'.print_r($manifest_cache,true).'</pre>'   ),'');
+       return $manifest_cache['version'];	
+	}
+
+public function getGithubRequests()
+{
+$mainframe =& JFactory::getApplication();    
+$paramsdata = JComponentHelper::getParams('com_sportsmanagement');
+//$mainframe->enqueueMessage(JText::_('getGithubRequests paramsdata<br><pre>'.print_r($paramsdata,true).'</pre>'   ),'');
+
+
+// Load the parameters
+        $uname = JComponentHelper::getParams('com_sportsmanagement')->get('cfg_github_username','diddipoeler');
+        $repo = JComponentHelper::getParams('com_sportsmanagement')->get('cfg_github_repository','sportsmanagement');
+		//$uname		= $paramsdata->get('cfg_github_username', '');
+		//$repo		= $paramsdata->get('cfg_github_repository', '');
+
+		// Convert the list name to a useable string for the JSON
+		if ($repo)
+		{
+			$frepo	= self::toAscii($repo);
+		}
+
+		// Initialize the array
+		$github	= array();
+
+		$req = 'https://api.github.com/repos/'.$uname.'/'.$frepo.'/commits';
+
+		// Fetch the decoded JSON
+		$obj = self::getJSON($req);
+
+		if (is_null($obj))
+		{
+			$github->error	= 'Error';
+			return $github;
+		}
+
+		// Process the filtering options and render the feed
+		$github = self::processData($obj, $paramsdata);
+
+		return $github;
+    
+}
+
+/**
+	 * Function to fetch a JSON feed
+	 *
+	 * @param   string  $req  The URL of the feed to load
+	 *
+	 * @return  array  The decoded JSON query
+	 *
+	 * @since	1.0
+	 */
+	static function getJSON($req)
+	{
+		// Create a new CURL resource
+		$ch = curl_init($req);
+
+		// Set options
+        
+		curl_setopt($ch, CURLOPT_HEADER, false);
+        $t_vers = curl_version();
+        curl_setopt($ch, CURLOPT_USERAGENT, 'curl/' . $t_vers['version'] );
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+		// Grab URL and pass it to the browser and store it as $json
+		$json = curl_exec($ch);
+
+		// Close CURL resource
+		curl_close($ch);
+
+		// Decode the fetched JSON
+		$obj = json_decode($json, true);
+
+		return $obj;
+	}
+
+/**
+	 * Function to process the GitHub data into a formatted object
+	 *
+	 * @param   array   $obj     The JSON data
+	 * @param   object  $params  The module parameters
+	 *
+	 * @return  array  An array of data for output
+	 *
+	 * @since   1.0
+	 */
+	static function processData($obj, $params)
+	{
+		// Initialize
+		$github = array();
+		$i = 0;
+
+		// Load the parameters
+        $uname = JComponentHelper::getParams('com_sportsmanagement')->get('cfg_github_username','');
+        $repo = JComponentHelper::getParams('com_sportsmanagement')->get('cfg_github_repository','');
+		//$uname		= $params->get('cfg_github_username', '');
+		//$repo		= $params->get('cfg_github_repository', '');
+		$count		= 15;
+
+		// Convert the list name to a useable string for the JSON
+		if ($repo)
+		{
+			$frepo	= self::toAscii($repo);
+		}
+
+		// Process the feed
+		foreach ($obj as $o)
+		{
+			if ($i <= $count)
+			{
+				// Initialize a new object
+				$github[$i]->commit	= new stdClass;
+
+				// The commit message linked to the commit
+				$github[$i]->commit->message = '<a href="https://github.com/'.$uname.'/'.$frepo.'/commit/'.$o['sha'].'" target="_blank" rel="nofollow">'.substr($o['sha'], 0, 7).'</a> - ';
+				$github[$i]->commit->message .= preg_replace("/#(\w+)/", '#<a href="https://github.com/'.$uname.'/'.$frepo.'/issues/\\1" target="_blank" rel="nofollow">\\1</a>', htmlspecialchars($o['commit']['message']));
+
+				// Check if the committer information
+				if ($o['author']['id'] != $o['committer']['id'])
+				{
+					// The committer name formatted with link
+					$github[$i]->commit->committer	= JText::_('COM_SPORTSMANAGEMENT_GITHUB_AND_COMMITTED_BY').'<a href="https://github.com/'.$o['committer']['login'].'" target="_blank" rel="nofollow">'.$o['commit']['committer']['name'].'</a>';
+
+					// The author wasn't the committer
+					$github[$i]->commit->author		= JText::_('COM_SPORTSMANAGEMENT_GITHUB_AUTHORED_BY');
+				}
+				else
+				{
+					// The author is also the committer
+					$github[$i]->commit->author		= JText::_('COM_SPORTSMANAGEMENT_GITHUB_COMMITTED_BY');
+				}
+
+				// The author name formatted with link
+				$github[$i]->commit->author .= '<a href="https://github.com/'.$o['author']['login'].'" target="_blank" rel="nofollow">'.$o['commit']['author']['name'].'</a>';
+
+				// The time of commit
+				$date = date_create($o['commit']['committer']['date']);
+				$date = date_format($date, 'r');
+				if ($params->get('relativeTime', '1') == '1')
+				{
+					$ISOtime = JHTML::date($date, 'Y-m-d H:i:s');
+
+					// Load the JavaScript; first ensure we have MooTools Core
+					JHtml::_('behavior.framework');
+					JHtml::script(JPATH_ADMINISTRATOR . '/components/com_joomleague/assets/js/prettydate.js', false, false);
+					$github[$i]->commit->time = ' <span class="commit-time" title="'.$ISOtime.'">'.JHtml::date($date, 'D M d H:i:s O Y').'</span>';
+				}
+				else
+				{
+					$github[$i]->commit->time = ' '.JHtml::date($date);
+				}
+
+				$i++;
+			}
+		}
+		return $github;
+	}
+    
+/**
+	 * Function to convert a formatted repo name into it's URL equivalent
+	 *
+	 * @param   string  $repo  The user inputted repo name
+	 *
+	 * @return  string  The repo name converted
+	 *
+	 * @since   1.0
+	 */
+	static function toAscii($repo)
+	{
+		$clean = preg_replace("/[^a-z'A-Z0-9\/_|+ -]/", '', $repo);
+		$clean = strtolower(trim($clean, '-'));
+		$repo  = preg_replace("/[\/_|+ -']+/", '-', $clean);
+
+		return $repo;
+	}    
+
+    
+}
+
+
+?>    
---
+++ administrator/components/com_joomleague/views/cpanel/tmpl/default.php
@@ -0,0 +1,238 @@
+<?php
+/**
+ * @category	Core
+ * @package		
+ * @copyright (C) 2013
+ * @license		GNU/GPL, see LICENSE.php
+ */
+ 
+;##################################################################
+;/* 
+;* Modified by 
+;*  
+;* email: 
+;* date: 2013
+;* Release: 1.0
+;* License : http://www.gnu.org/copyleft/gpl.html GNU/GPL 
+;*/
+################################################################### 
+ 
+// Disallow direct access to this file
+defined('_JEXEC') or die('Restricted access');
+?>
+<table width="100%" border="0">
+	<tr>
+		<td width="55%" valign="top">
+			<div id="cpanel">
+				<?php echo $this->addIcon('icon-48-clubs.png','index.php?option=com_joomleague&view=clubs', JText::_('COM_SPORTSMANAGEMENT_D_MENU_CLUBS'));?>
+				<?php echo $this->addIcon('icon-48-teams.png','index.php?option=com_joomleague&view=teams', JText::_('COM_SPORTSMANAGEMENT_D_MENU_TEAMS'));?>
+				<?php echo $this->addIcon('icon-48-leagues.png','index.php?option=com_joomleague&view=leagues', JText::_('COM_SPORTSMANAGEMENT_D_MENU_LEAGUES'));?>
+				<?php echo $this->addIcon('icon-48-seasons.png','index.php?option=com_joomleague&view=seasons', JText::_('COM_SPORTSMANAGEMENT_D_MENU_SEASONS'));?>
+				<?php echo $this->addIcon('icon-48-playground.png','index.php?option=com_joomleague&view=playgrounds', JText::_('COM_SPORTSMANAGEMENT_D_MENU_VENUES'));?>
+				<?php echo $this->addIcon('icon-48-positions.png','index.php?option=com_joomleague&view=positions', JText::_('COM_SPORTSMANAGEMENT_D_MENU_POSITIONS'));?>
+				<?php echo $this->addIcon('icon-48-statistics.png','index.php?option=com_joomleague&view=statistics', JText::_('COM_SPORTSMANAGEMENT_D_MENU_STATISTICS'));?>
+				<?php echo $this->addIcon('icon-48-sportstypes.png','index.php?option=com_joomleague&view=sportstypes', JText::_('COM_SPORTSMANAGEMENT_D_MENU_SPORTSTYPES'));?>
+				<?php echo $this->addIcon('icon-48-events.png','index.php?option=com_joomleague&view=eventtypes', JText::_('COM_SPORTSMANAGEMENT_D_MENU_EVENTS'));?>
+        <?php echo $this->addIcon('groups.gif','index.php?option=com_joomleague&view=persons', JText::_('COM_SPORTSMANAGEMENT_D_MENU_PERSONS'));?>
+        <?php echo $this->addIcon('icon-48-agegroups.png','index.php?option=com_joomleague&view=agegroups', JText::_('COM_SPORTSMANAGEMENT_D_MENU_AGEGROUPS'));?>
+        <?php echo $this->addIcon('world.png','index.php?option=com_joomleague&view=jlextcountries', JText::_('COM_SPORTSMANAGEMENT_D_MENU_COUNTRIES'));?>
+        <?php echo $this->addIcon('association.jpg','index.php?option=com_joomleague&view=jlextassociations', JText::_('COM_SPORTSMANAGEMENT_D_MENU_ASSOCIATIONS'));?>
+        <?php echo $this->addIcon('applications.gif','index.php?option=com_joomleague&view=extrafields', JText::_('COM_SPORTSMANAGEMENT_D_MENU_EXTRAFIELDS'));?>
+				
+				
+			</div>
+		</td>
+		<td width="45%" valign="top">
+		
+			<div style=" top:1px;  width:auto; background-color:#FFFFE0; border:4px ridge #46CD29; padding:10px; margin:5px">
+				<table width="99%">
+					<tr align="center">
+						<td width="50%" >
+							<div style="text-align: center;">
+							<?php echo $this->addIcon('cbehome.png','http://www.fussballineuropa.de', JText::_('COM_SPORTSMANAGEMENT_SITE_LINK'), true); ?>
+							</div>
+						</td>
+						<td width="50%" >
+							<div style="text-align: center;">
+							<?php echo $this->addIcon('cbehelp.png','http://joomleaguewiki.grammatikas-grill.de/index.php/Hauptseite', JText::_('COM_SPORTSMANAGEMENT_HELP_LINK'), true); ?>
+							</div>
+						</td>					
+					</tr>
+				</table>
+			</div>			
+
+		
+			<?php
+				echo $this->pane->startPane( 'stat-pane' );
+				echo $this->pane->startPanel( JText::_('COM_SPORTSMANAGEMENT_WELCOME_TO_CBECOMMUNITY') , 'welcome' );
+			?>
+			<table class="adminlist">
+				<tr>
+					<td>
+						<div style="font-weight:700;">
+							<?php echo JText::_('COM_SPORTSMANAGEMENT_GREAT_COMPONENT_MSG');?>
+						</div>
+						<p>
+							
+						</p>
+						<p>
+							
+						</p>
+					</td>
+				</tr>
+			</table>
+			<?php
+				echo $this->pane->endPanel();
+				echo $this->pane->startPanel( JText::_('COM_SPORTSMANAGEMENT_STATISTICS') , 'cbe' );
+			?>
+				<table class="adminlist">
+					<tr>
+						<td>
+							<?php echo JText::_( 'COM_SPORTSMANAGEMENT_TOTAL_USERS' ).': '; ?>
+						</td>
+						<td align="center">
+							<strong><?php echo $this->cbe->total; ?></strong>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<?php echo JText::_( 'COM_SPORTSMANAGEMENT_TOTAL_BLOCKED_USERS' ).': '; ?>
+						</td>
+						<td align="center">
+							<strong><?php echo $this->cbe->blocked; ?></strong>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<?php echo JText::_( 'COM_SPORTSMANAGEMENT_TOTAL_APPLICATIONS_INSTALLED' ).': '; ?>
+						</td>
+						<td align="center">
+							<strong><?php echo $this->cbe->applications; ?></strong>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<?php echo JText::_( 'COM_SPORTSMANAGEMENT_TOTAL_ACTIVITY_UPDATES' ).': '; ?>
+						</td>
+						<td align="center">
+							<strong><?php echo $this->cbe->updates; ?></strong>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<?php echo JText::_( 'COM_SPORTSMANAGEMENT_PHOTOS_TOTAL' ).': '; ?>
+						</td>
+						<td align="center">
+							<strong><?php echo $this->cbe->photos; ?></strong>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<?php echo JText::_( 'COM_SPORTSMANAGEMENT_VIDEOS_TOTAL' ).': '; ?>
+						</td>
+						<td align="center">
+							<strong><?php echo $this->cbe->videos; ?></strong>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<?php echo JText::_( 'COM_SPORTSMANAGEMENT_GROUPS_T0TAL_DISCUSSIONS' ).': '; ?>
+						</td>
+						<td align="center">
+							<strong><?php echo $this->cbe->groupDiscussion; ?></strong>
+						</td>
+					</tr>
+				</table>
+
+			<?php
+				echo $this->pane->endPanel();
+				echo $this->pane->startPanel( JText::_('COM_SPORTSMANAGEMENT_GROUPS_STATISTICS'), 'groups' );
+			?>
+				<table class="adminlist">
+					<tr>
+						<td>
+							<?php echo JText::_( 'COM_SPORTSMANAGEMENT_GROUPS_PUBLISHED' ).': '; ?>
+						</td>
+						<td align="center">
+							<strong><?php echo $this->groups->published; ?></strong>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<?php echo JText::_( 'COM_SPORTSMANAGEMENT_GROUPS_UNPUBLISHED' ).': '; ?>
+						</td>
+						<td align="center">
+							<strong><?php echo $this->groups->unpublished; ?></strong>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<?php echo JText::_( 'COM_SPORTSMANAGEMENT_GROUP_CATEGORIES' ).': '; ?>
+						</td>
+						<td align="center">
+							<strong><?php echo $this->groups->categories; ?></strong>
+						</td>
+					</tr>
+				</table>
+			<?php
+				echo $this->pane->endPanel();
+                
+				echo $this->pane->startPanel( JText::_('COM_SPORTSMANAGEMENT_GITHUB_REQUESTS') , 'GITHUBREQUESTS' );
+			?>
+			<table class="adminlist">
+				<tr>
+					<td>
+
+<ul class="GH-commit<?php echo $moduleclass_sfx;?>">
+	<?php foreach ($this->githubrequest  as $o) { ?>
+	<li><?php echo $o->commit->message.$o->commit->author;
+	if (isset($o->commit->committer)) {
+		echo $o->commit->committer;
+	}
+	echo $o->commit->time; ?>
+	</li>
+	<?php } ?>
+</ul>
+						
+					</td>
+				</tr>
+			</table>
+			<?php
+				echo $this->pane->endPanel();	
+				echo $this->pane->startPanel( JText::_('COM_SPORTSMANAGEMENT_WELCOME_TO_FORUM') , 'FORUM' );
+			?>
+			<table class="adminlist">
+				<tr>
+					<td>
+
+					coming soon
+						
+					</td>
+				</tr>
+			</table>
+			<?php
+				echo $this->pane->endPanel();	
+								
+				echo $this->pane->endPane();
+			?>
+		</td>
+	</tr>
+	<!-- FOOTER INFO DASHBOARD TODO ALL PAGES -->
+	<tr>
+		<td style="text-align: left; width: 33%;">
+			<a href="http://www.facebook.com/pages/Sportsmanager/558711710835555" target="_blank"><?php echo JText::_( "COM_SPORTSMANAGEMENT_FACEBOOK_FOLLOW" ); ?></a>
+			<br/>
+			<a href="https://github.com/diddipoeler/sportsmanagement" target="_blank"><?php echo JText::_( "COM_SPORTSMANAGEMENT_GITHUB_FOLLOW" ); ?></a>
+			
+		</td>
+		<td style="text-align: center; width: 33%;">
+			<?php echo JText::_( "COM_SPORTSMANAGEMENT_DESC" ); ?>
+			<br/>
+			<?php echo JText::_( "COM_SPORTSMANAGEMENT_COPYRIGHT" ); ?>: &copy; <a href="http://www.fussballineuropa.de" target="_blank">Fussball in Europa</a>
+			<br/>
+			<?php echo JText::_( "COM_SPORTSMANAGEMENT_VERSION" ); ?>: <?php echo JText::sprintf( 'Version: %1$s', $this->version ); ?>
+		</td>
+		<td style="text-align: right; width: 33%;">
+		</td>
+	</tr>
+</table>
