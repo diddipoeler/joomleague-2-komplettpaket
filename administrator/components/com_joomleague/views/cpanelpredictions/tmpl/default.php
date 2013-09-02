e9c9fd537ea9e9fbaa845be91b221c93a706fc79
diddipoeler <famploeger@arcor.de>
2013-09-02 10:31:25 +0200

    administrator: cpanel predictiongame

---
+++ administrator/components/com_joomleague/views/cpanelpredictions/tmpl/default.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @category	Core
+ * @package		
+ * @copyright (C) 2013
+ * @license		GNU/GPL, see LICENSE.php
+ */
+ 
+;##################################################################
+;/* 
+;* Modified by 
+;*  
+;* email: 
+;* date: 2013
+;* Release: 1.0
+;* License : http://www.gnu.org/copyleft/gpl.html GNU/GPL 
+;*/
+################################################################### 
+ 
+// Disallow direct access to this file
+defined('_JEXEC') or die('Restricted access');
+?>
+<table width="100%" border="0">
+	<tr>
+		<td width="100%" valign="top">
+			<div id="cpanel">
+				<?php echo $this->addIcon('dfb-key.jpg','index.php?option=com_joomleague&view=predictiongames', JText::_('COM_JOOMLEAGUE_PREDICTION'));?>
+        		
+                
+
+			</div>
+		</td>
+		
+	</tr>
+	<!-- FOOTER INFO DASHBOARD TODO ALL PAGES -->
+	<tr>
+		<td style="text-align: left; width: 50%;">
+			<a href="http://www.facebook.com/pages/Sportsmanager/558711710835555" target="_blank"><?php echo JText::_( "COM_JOOMLEAGUE_FACEBOOK_FOLLOW" ); ?></a>
+			<br/>
+			<a href="https://github.com/diddipoeler/sportsmanagement" target="_blank"><?php echo JText::_( "COM_JOOMLEAGUE_GITHUB_FOLLOW" ); ?></a>
+			<br/>				
+			<a href="http://gplus.to/JoomlaCBE" target="_blank"><?php echo JText::_( "COM_JOOMLEAGUE_GPLUS_FOLLOW" ); ?></a>
+			<br/>
+			<a href="http://extensions.joomla.org/extensions/owner/JoomlaCBE" target="_blank"><?php echo JText::_( "COM_JOOMLEAGUE_JED_FEEDBACK" ); ?></a>
+		</td>
+		<td style="text-align: left; width: 50%;">
+			<?php echo JText::_( "COM_JOOMLEAGUE_CBE_DESC" ); ?>
+			<br/>
+			<?php echo JText::_( "COM_JOOMLEAGUE_COPYRIGHT" ); ?>: &copy; <a href="http://www.fussballineuropa.de" target="_blank">Fussball in Europa</a>
+			<br/>
+			<?php echo JText::_( "COM_JOOMLEAGUE_VERSION" ); ?>: <?php echo JText::sprintf( 'Version: %1$s', $this->version ); ?>
+		</td>
+		
+	</tr>
+</table>
